37e999be71b33f4f738af25edaf6151b5f3d4019
Felipe Augusto <felipeaugusto030412@gmail.com>
2024-08-30 19:07:59 -0300

    adição do exercicio 5

--- exercicio4.js
+++ exercicio4.js
@@ -1,1 +1,20 @@
 //por: Felipe Augusto Pereira Lemes
+var minhaVariavel='GLOBAL';
+outraVariavel='GLOBAL';
+
+function minhaFuncao(){
+    var minhaVariavel='LOCAL';
+    return minhaVariavel;
+}
+
+function outraFuncao(){
+    outraVariavel='LOCAL';
+    return outraVariavel;
+}
+
+console.log('minhaVariavel:' + minhaVariavel);
+console.log('minhaFuncao:' + minhaFuncao());
+console.log('outraVariavel:' + outraVariavel);
+console.log('outraFuncao:' + outraFuncao());
+
+
---
+++ exercicio5.js
@@ -0,0 +1,11 @@
+//por:Felipe Augusto Pereira Lemes
+
+// Exemplos de operadores mais simples
+let a = 10;
+let b = 5;
+let soma = a + b;
+console.log('A soma de ' + a + ' + ' + b + ' é: ' + soma);
+
+// Operador de Comparação
+let maiorQue = a > b;
+console.log(a + ' é maior que ' + b + ': ' + maiorQue);
